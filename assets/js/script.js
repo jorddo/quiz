1fcb0667d9f84d77642aa8187c0269b525d631c9
jorddo <jordanbsneed@gmail.com>
2022-01-15 18:03:52 -0600

    added timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 const question1 = document.querySelector('.question1');
+const startPage = document.querySelector('.start-page');
 
+// toggle question 1 "on"
 function turnOn1() {
   question1.classList.toggle('hide');
+}
+
+// toggle start page "off"
+function turnOff1() {
+  startPage.classList.add('hide');
+}
+
+// timer
+let time = 59;
+function minTimer() {
+  document.getElementById('timer').innerHTML = time;
+  time--;
+  if (time < 0) {
+    alert(`Sorry! You're out of time!`);
+  } else {
+    setTimeout(minTimer, 1000);
+  }
 }
