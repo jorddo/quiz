0762f3a22574ba72c670602ecdce1bbfcd637a94
jorddo <jordanbsneed@gmail.com>
2022-01-18 14:06:48 -0600

    adding buttons and navigation through the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,44 @@
-const question1 = document.querySelector('.question1');
 const startPage = document.querySelector('.start-page');
+const question1 = document.querySelector('.question1');
+const question2 = document.querySelector('.question2');
+const question3 = document.querySelector('.question3');
+const question4 = document.querySelector('.question4');
+const question5 = document.querySelector('.question5');
 
-// toggle question 1 "on"
+// turn off start page
+function turnOff() {
+  startPage.classList.add('hide');
+}
+
+// render question 1 and hide it after interaction
 function turnOn1() {
   question1.classList.toggle('hide');
 }
 
-// toggle start page "off"
 function turnOff1() {
-  startPage.classList.add('hide');
+  question1.classList.add('hide');
 }
+
+// render question 2
+function turnOn2() {
+  question2.classList.toggle('hide');
+}
+
+function turnOff2() {
+  question2.classList.add('hide');
+}
+// render question 3
+function turnOn3() {
+  question3.classList.toggle('hide');
+}
+
+// render question 4
+
+// render question 5
+
+// done page/ initial entry
+
+// high score/ leaderboard page
 
 // timer
 let time = 59;
